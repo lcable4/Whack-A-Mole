a3f7e45658d28516c14556e504f7d06c6576b9e4
Logan <logancable@ymail.com>
2023-02-03 08:52:05 -0800

    finished with game, everything is working

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 let score = 0;
+const scoreDisplay = document.getElementById('score');
+const holes = document.getElementsByClassName("hole");
+const gameArea = document.getElementById('whack-a-mole');
+
+// Targets a random hole and toggles the mole in that specific div
+setInterval(function () {
+    let holeIndex = Math.floor(Math.random() * holes.length);
+    holes[holeIndex].classList.toggle("mole")
+}, 300);
+gameArea.addEventListener('click', function(clickEvent) {
+  if (clickEvent.target.matches('.mole')) {
+    clickEvent.target.classList.remove('mole');
+    score++;
+    scoreDisplay.innerText = score;    
+    // we hit a mole!
+  }
+});
